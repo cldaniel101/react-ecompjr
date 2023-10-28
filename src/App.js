1deba8b28db677307a1541ba3f3519b1d39afe60
Cláudio Peruna <danielperuna2012@gmail.com>
2023-10-28 15:58:05 -0300

    Update Components
    entrada + quemSomos + cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import Header from "./components/header/header";
+import Entrada from "./components/entrada/entrada";
+import QuemSomos from "./components/quemSomos/quemSomos";
+import Cards from "./components/cards/cards";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <main>
+        <Entrada/>
+        <QuemSomos/>
+        <Cards/>
+      </main>
     </div>
   );
 }
